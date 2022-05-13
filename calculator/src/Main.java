10c81bfdd451b3e900665c5d1c1e74f8450095fd
benbr <0130@htl.rennweg.at>
2022-05-13 10:29:06 +0200

    configured JavaFx

--- calculator/src/Main.java
+++ calculator/src/Main.java
@@ -1,2 +1,13 @@
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
 }
