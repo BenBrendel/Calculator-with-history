738333e342d13265eabef451eff50ce058307fe9
benbr <0130@htl.rennweg.at>
2022-05-19 18:35:56 +0200

    finished calculator UI

--- calculator/src/Main.java
+++ calculator/src/Main.java
@@ -6,7 +6,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -25,29 +29,31 @@ public class Main extends Application {
         Scene scene = new Scene(gridPane, 250, 150);
         stage.setScene(scene);
 
+        gridPane.setBackground(new Background(new BackgroundFill(Color.rgb(200, 200, 200), CornerRadii.EMPTY, Insets.EMPTY)));
         gridPane.setHgap(10);
         gridPane.setVgap(10);
         gridPane.setPadding(new Insets(10));
 
+
         Label label1 = new Label("Wählen Sie, ob Sie einen Verlauf laden oder eine neue Session erstellen wollen:");
         Button button1 = new Button("neue Session");
-        TextField textfield1 = new TextField();
-        textfield1.setPromptText("Verlaufs-Pfad:");
+        TextField textField1 = new TextField();
+        textField1.setPromptText("Verlaufs-Pfad:");
 
         label1.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
         button1.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-        textfield1.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        textField1.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
 
         gridPane.add(label1, 0, 0);
         gridPane.add(button1, 0, 1);
-        gridPane.add(textfield1, 0, 3);
+        gridPane.add(textField1, 0, 3);
 
         button1.setOnMouseClicked(k -> {
             Calculator calc = new Calculator();
             stage.setScene(calc.newCalc());
         });
 
-        textfield1.setOnKeyPressed(k -> {
+        textField1.setOnKeyPressed(k -> {
             if (k.getCode().equals(KeyCode.ENTER)) {
                 Calculator calc = new Calculator();
                 stage.setScene(calc.newCalc());
